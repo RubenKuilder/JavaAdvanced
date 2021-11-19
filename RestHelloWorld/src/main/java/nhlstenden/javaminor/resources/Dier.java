9a70343373b9f7dca80ac21d20097224fecd949b
Ruben <rubenkuilder@gmail.com>
2021-11-20 00:27:07 +0100

    More work on the dierController

--- RestHelloWorld/src/main/java/nhlstenden/javaminor/resources/Dier.java
+++ RestHelloWorld/src/main/java/nhlstenden/javaminor/resources/Dier.java
@@ -25,4 +25,36 @@ public class Dier {
         this.name = name;
         this.age = age;
     }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- RestHelloWorld/src/main/java/nhlstenden/javaminor/resources/DierController.java
+++ RestHelloWorld/src/main/java/nhlstenden/javaminor/resources/DierController.java
@@ -1,15 +1,50 @@
 package nhlstenden.javaminor.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/dieren")
 public class DierController {
-    @GetMapping
-    public String getDieren() {
-        return "Hello dieren!";
-//        return new Dier("Dog", "Podenco Andaluz", "Koen", 3);
+    ObjectMapper mapper = new ObjectMapper();
+
+    HashMap<Integer, Dier> dieren = new HashMap<>();
+
+    public DierController() {
+        this.dieren.put(0, new Dier("Dog", "Podenco Andaluz", "Koen", 3));
+        this.dieren.put(1, new Dier("Dog", "Podenco Canario", "Olivier", 11));
+        this.dieren.put(2, new Dier("Dog", "Labrador", "Katja", 5));
+        this.dieren.put(3, new Dier("Cat", "", "Joris", 7));
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getDieren() throws JsonProcessingException {
+        return mapper.writeValueAsString(dieren);
+    }
+
+    @GetMapping("/{id}")
+    public String getDierById(@PathVariable int id) throws JsonProcessingException {
+        Dier dier = dieren.get(id);
+
+        return mapper.writeValueAsString(dier);
+    }
+
+    @GetMapping(params = "name")
+    public String getDierByName(@RequestParam String name) throws JsonProcessingException {
+        ArrayList<Dier> dierenMetNamen = new ArrayList<>();
+
+        for(Map.Entry<Integer, Dier> entry : dieren.entrySet()) {
+            if(entry.getValue().getName().contains(name)) {
+                dierenMetNamen.add(entry.getValue());
+            }
+        }
+
+        return mapper.writeValueAsString(dierenMetNamen);
     }
 }
